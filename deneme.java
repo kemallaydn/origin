5eeab83d82f34a7c02c74fdf6b183338f27d530f
kemallaydn <kemal.aydin3429@gmail.com>
2024-03-09 13:31:16 +0300

    inital commit

---
+++ deneme.java
@@ -0,0 +1,8 @@
+public  deneme {
+        public static void main(String[] args) {
+            try {
+                System.out.println("Dosya indiriliyor...");
+
+                // GitHub reposunun URL'si
+                String repoUrl = "
+}
---
+++ main.java
@@ -0,0 +1,9 @@
+public class main {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("Dosya indiriliyor...");
+
+            // GitHub reposunun URL'si
+            String repoUrl = "
+}
